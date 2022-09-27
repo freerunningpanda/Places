32517aecba26fb77d9c23d0f796520c9250d6501
Aleksandr Nagornyy <freerunningpanda@yandex.ru>
2022-09-27 12:48:05 +0300

    released delete items

--- lib/mocks.dart
+++ lib/mocks.dart
@@ -72,6 +72,134 @@ abstract class Mocks {
     ),
   ];
 
+  static final List<Sight> sightsTovisit = [
+    Sight(
+      name: 'Мытищинский парк',
+      lat: 55.911397,
+      lot: 37.740033,
+      url: 'https://pic.rutubelist.ru/video/39/09/390905576c021a02b5b57c374ba16621.jpg',
+      details: 'Мытищинский парк — центральный парк в одноименном городе Московской области.',
+      type: AppString.park,
+    ),
+    Sight(
+      name: 'Воронцовский дворец',
+      lat: 44.419881,
+      lot: 34.055799,
+      url: 'https://core-pht-proxy.maps.yandex.ru/v1/photos/download?photo_id=deRmH1T9ukwAqPbuaHG6FQ&image_size=XXL',
+      details:
+          'Парк на территории Алупки. Памятник садово-паркового искусства, основан в первой половине XIX века под руководством немецкого садовника Карла Кебаха. Составляет единый ансамбль с Воронцовским дворцом.',
+      type: AppString.park,
+    ),
+    Sight(
+      name: 'Мытищинский историко-художественный музей',
+      lat: 55.911355,
+      lot: 37.735425,
+      url: 'https://mytyshimuseum.ru/wp-content/uploads/Zal-19-vek-2-1.jpeg',
+      details: 'Центральный музей городского округа Мытищи. Открыт в 1962 году.',
+      type: AppString.museum,
+    ),
+    Sight(
+      name: 'Папа Джонс',
+      lat: 55.908155,
+      lot: 37.730916,
+      url: 'https://avatars.mds.yandex.net/get-altay/4398559/2a00000181b14c1ff7099db35afff1bef259/XXXL',
+      details:
+          'Пиццерия. Предзаказ онлайн, оплата картой, еда навынос, средний счёт 600–1200 ₽, доставка еды. Цены: средние.',
+      type: AppString.cafe,
+    ),
+    Sight(
+      name: 'Якитория',
+      lat: 55.917942,
+      lot: 37.725245,
+      url: 'https://static.yakitoriya.ru/media/cache/60/15/60158655420e2922294478c35d86ba77.png',
+      details:
+          'Летняя веранда, доставка еды, роллы филадельфия от 537 ₽, бесплатная доставка, оплата картой, доставка, Wi-Fi, еда навынос, детское меню, средний счёт от 1500 ₽, детская комната..',
+      type: AppString.restaurant,
+    ),
+    Sight(
+      name: 'Lecco',
+      lat: 55.907089,
+      lot: 37.718347,
+      url:
+          'https://cf.bstatic.com/xdata/images/hotel/max1280x900/33106839.jpg?k=6b68980e0b040d0e675237bb26a2f08ce9cc2db5cf404ed965edc5c585240b37&o=&hp=1',
+      details:
+          'Отель Lecco расположен в тихом жилом районе, менее чем в 3 км от центра Мытищ и всего в 40 минутах езды от аэропорта Шереметьево. Кроме того, в отеле работает оздоровительный центр и салон красоты.',
+      type: AppString.hotel,
+    ),
+    Sight(
+      name: '13-й микрорайон',
+      lat: 55.903283,
+      lot: 37.714688,
+      url: 'https://core-pht-proxy.maps.yandex.ru/v1/photos/download?photo_id=BavQsmxTMiOC0OpsXpM1Rg&image_size=XXXL',
+      details: 'Ограничен улицей Сукромка, улицей Благовещенская, рекой Яуза',
+      type: AppString.particularPlace,
+    ),
+  ];
+
+  static final List<Sight> visitedSights = [
+    Sight(
+      name: 'Мытищинский парк',
+      lat: 55.911397,
+      lot: 37.740033,
+      url: 'https://pic.rutubelist.ru/video/39/09/390905576c021a02b5b57c374ba16621.jpg',
+      details: 'Мытищинский парк — центральный парк в одноименном городе Московской области.',
+      type: AppString.park,
+    ),
+    Sight(
+      name: 'Воронцовский дворец',
+      lat: 44.419881,
+      lot: 34.055799,
+      url: 'https://core-pht-proxy.maps.yandex.ru/v1/photos/download?photo_id=deRmH1T9ukwAqPbuaHG6FQ&image_size=XXL',
+      details:
+          'Парк на территории Алупки. Памятник садово-паркового искусства, основан в первой половине XIX века под руководством немецкого садовника Карла Кебаха. Составляет единый ансамбль с Воронцовским дворцом.',
+      type: AppString.park,
+    ),
+    Sight(
+      name: 'Мытищинский историко-художественный музей',
+      lat: 55.911355,
+      lot: 37.735425,
+      url: 'https://mytyshimuseum.ru/wp-content/uploads/Zal-19-vek-2-1.jpeg',
+      details: 'Центральный музей городского округа Мытищи. Открыт в 1962 году.',
+      type: AppString.museum,
+    ),
+    Sight(
+      name: 'Папа Джонс',
+      lat: 55.908155,
+      lot: 37.730916,
+      url: 'https://avatars.mds.yandex.net/get-altay/4398559/2a00000181b14c1ff7099db35afff1bef259/XXXL',
+      details:
+          'Пиццерия. Предзаказ онлайн, оплата картой, еда навынос, средний счёт 600–1200 ₽, доставка еды. Цены: средние.',
+      type: AppString.cafe,
+    ),
+    Sight(
+      name: 'Якитория',
+      lat: 55.917942,
+      lot: 37.725245,
+      url: 'https://static.yakitoriya.ru/media/cache/60/15/60158655420e2922294478c35d86ba77.png',
+      details:
+          'Летняя веранда, доставка еды, роллы филадельфия от 537 ₽, бесплатная доставка, оплата картой, доставка, Wi-Fi, еда навынос, детское меню, средний счёт от 1500 ₽, детская комната..',
+      type: AppString.restaurant,
+    ),
+    Sight(
+      name: 'Lecco',
+      lat: 55.907089,
+      lot: 37.718347,
+      url:
+          'https://cf.bstatic.com/xdata/images/hotel/max1280x900/33106839.jpg?k=6b68980e0b040d0e675237bb26a2f08ce9cc2db5cf404ed965edc5c585240b37&o=&hp=1',
+      details:
+          'Отель Lecco расположен в тихом жилом районе, менее чем в 3 км от центра Мытищ и всего в 40 минутах езды от аэропорта Шереметьево. Кроме того, в отеле работает оздоровительный центр и салон красоты.',
+      type: AppString.hotel,
+    ),
+    Sight(
+      name: '13-й микрорайон',
+      lat: 55.903283,
+      lot: 37.714688,
+      url: 'https://core-pht-proxy.maps.yandex.ru/v1/photos/download?photo_id=BavQsmxTMiOC0OpsXpM1Rg&image_size=XXXL',
+      details: 'Ограничен улицей Сукромка, улицей Благовещенская, рекой Яуза',
+      type: AppString.particularPlace,
+    ),
+  ];
+
   static double startPoint = 2000;
   static double endPoint = 8000;
 
--- lib/ui/screens/visiting_screen/visiting_screen.dart
+++ lib/ui/screens/visiting_screen/visiting_screen.dart
@@ -8,11 +8,11 @@ import 'package:places/ui/res/app_strings.dart';
 import 'package:places/ui/res/app_typography.dart';
 import 'package:places/ui/screens/res/custom_colors.dart';
 import 'package:places/ui/screens/sight_card/sight_card.dart';
-import 'package:places/ui/screens/sight_details/sight_details.dart';
 import 'package:places/ui/widgets/add_new_place_button.dart';
 import 'package:places/ui/widgets/sight_icons.dart';
 
-List<Sight> list = Mocks.mocks;
+List<Sight> sightsToVisit = Mocks.sightsTovisit;
+List<Sight> visitedSights = Mocks.visitedSights;
 
 class VisitingScreen extends StatefulWidget {
   const VisitingScreen({Key? key}) : super(key: key);
@@ -41,14 +41,14 @@ class _VisitingScreenState extends State<VisitingScreen> {
                     padding: const EdgeInsets.only(top: 30),
                     child: TabBarView(
                       children: [
-                        if (list.isNotEmpty)
+                        if (sightsToVisit.isNotEmpty)
                           const _WantToVisitWidget()
                         else
                           const _EmptyList(
                             icon: AppAssets.card,
                             description: AppString.likedPlaces,
                           ),
-                        if (list.isNotEmpty)
+                        if (visitedSights.isNotEmpty)
                           const _VisitedWidget()
                         else
                           const _EmptyList(
@@ -73,8 +73,6 @@ class _VisitingScreenState extends State<VisitingScreen> {
     );
   }
 }
-
-
 
 class _AppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
@@ -144,19 +142,26 @@ class _TabBarWidget extends StatelessWidget {
   }
 }
 
-class _WantToVisitWidget extends StatelessWidget {
+class _WantToVisitWidget extends StatefulWidget {
   const _WantToVisitWidget({Key? key}) : super(key: key);
 
+  @override
+  State<_WantToVisitWidget> createState() => _WantToVisitWidgetState();
+}
+
+class _WantToVisitWidgetState extends State<_WantToVisitWidget> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
     return ListView.builder(
-      itemCount: list.length,
+      itemCount: sightsToVisit.length,
       itemBuilder: (context, index) {
-        final item = Mocks.mocks[index];
+        final item = sightsToVisit[index];
 
         return SightCard(
+          // key: ValueKey(sightsToVisit[index]),
+          removeSight: () => deleteSight(index, sightsToVisit),
           isVisitingScreen: true,
           item: item,
           url: item.url ?? 'no_url',
@@ -198,73 +203,80 @@ class _WantToVisitWidget extends StatelessWidget {
       },
     );
   }
+
+  void deleteSight(int index, List<Sight> sightList) {
+    setState(() {
+      sightList.removeAt(index);
+    });
+  }
 }
 
-class _VisitedWidget extends StatelessWidget {
+class _VisitedWidget extends StatefulWidget {
   const _VisitedWidget({Key? key}) : super(key: key);
 
+  @override
+  State<_VisitedWidget> createState() => _VisitedWidgetState();
+}
+
+class _VisitedWidgetState extends State<_VisitedWidget> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
     return ListView.builder(
-      itemCount: list.length,
+      itemCount: visitedSights.length,
       itemBuilder: (context, index) {
-        final sight = Mocks.mocks[index];
+        final item = visitedSights[index];
 
-        return GestureDetector(
-          onTap: () {
-            debugPrint('🟡---------go to SightDetails pressed');
-            Navigator.of(context).push(
-              MaterialPageRoute<SightDetails>(
-                builder: (context) => SightDetails(
-                  sight: sight,
-                ),
-              ),
-            );
-          },
-          child: SightCard(
-            isVisitingScreen: true,
-            item: sight,
-            url: sight.url,
-            type: sight.type,
-            name: sight.name,
-            aspectRatio: AppCardSize.visitingCard,
-            details: [
-              Text(
-                sight.name,
-                maxLines: 2,
-                style: theme.textTheme.headlineSmall,
-              ),
-              const SizedBox(height: 2),
-              const Text(
-                '${AppString.targetReach} 12 окт. 2022',
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-                style: AppTypography.detailsText,
-              ),
-              const SizedBox(height: 10),
-              const Text(
-                '${AppString.closed} 09:00',
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-                style: AppTypography.detailsText,
-              ),
-            ],
-            actionOne: const SightIcons(
-              assetName: AppAssets.share,
-              width: 24,
-              height: 24,
+        return SightCard(
+          // key: ValueKey(sightsToVisit[index]),
+          removeSight: () => deleteSight(index, visitedSights),
+          isVisitingScreen: true,
+          item: item,
+          url: item.url ?? 'no_url',
+          type: item.type,
+          name: item.name,
+          aspectRatio: AppCardSize.visitingCard,
+          details: [
+            Text(
+              item.name,
+              maxLines: 2,
+              style: theme.textTheme.headlineSmall,
             ),
-            actionTwo: const SightIcons(
-              assetName: AppAssets.cross,
-              width: 22,
-              height: 22,
+            const SizedBox(height: 2),
+            const Text(
+              '${AppString.planning} 12 окт. 2022',
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: AppTypography.greenColor,
             ),
+            const SizedBox(height: 10),
+            const Text(
+              '${AppString.closed} 09:00',
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: AppTypography.textText16Regular,
+            ),
+          ],
+          actionOne: const SightIcons(
+            assetName: AppAssets.calendarWhite,
+            width: 24,
+            height: 24,
+          ),
+          actionTwo: const SightIcons(
+            assetName: AppAssets.cross,
+            width: 22,
+            height: 22,
           ),
         );
       },
     );
+  }
+
+  void deleteSight(int index, List<Sight> sightList) {
+    setState(() {
+      sightList.removeAt(index);
+    });
   }
 }
 
