9dafeff6fba7d2a1f7b70003b09e85557af79730
Aleksandr Nagornyy <freerunningpanda@yandex.ru>
2023-01-21 18:45:55 +0700

    written app_state

---
+++ lib/redux/state/search_screen_state.dart
@@ -0,0 +1,18 @@
+import 'package:equatable/equatable.dart';
+
+import 'package:places/data/model/place.dart';
+
+class SearchScreenState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class SearchScreenEmptyState extends SearchScreenState {}
+
+class SeacrhScreenFindedPlacesState extends SearchScreenState {
+  final Set<Place> places;
+  
+  SeacrhScreenFindedPlacesState({
+    required this.places,
+  });
+}
