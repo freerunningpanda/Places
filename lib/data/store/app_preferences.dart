61e9a14552651592622be0cffcd070bdbed63c2a
Aleksandr Nagornyy <freerunningpanda@yandex.ru>
2023-03-16 01:01:52 +0700

    implemented shared_prefs to the distance_slider

--- lib/data/store/app_preferences.dart
+++ lib/data/store/app_preferences.dart
@@ -5,13 +5,31 @@ import 'package:shared_preferences/shared_preferences.dart';
 class AppPreferences extends Store {
   static late SharedPreferences _prefs;
 
-  static Future init() async => _prefs = await SharedPreferences.getInstance().then(
+  static Future<SharedPreferences> init() async => _prefs = await SharedPreferences.getInstance().then(
         (prefs) => _prefs = prefs,
       );
 
-  static Future setStartValue(double start) async => _prefs.setDouble(rangeValueStart, start);
-  static Future setEndValue(double end) async => _prefs.setDouble(rangeValueEnd, end);
+  static Future<bool> setStartValue(double start) async {
+    final prefs = await _prefs.setDouble(rangeValueStart, start);
 
-  static double getStartValue() => _prefs.getDouble(rangeValueStart) ?? 100.0;
-  static double getEndValue() => _prefs.getDouble(rangeValueEnd) ?? 10000.0;
+    return prefs;
+  }
+
+  static Future<bool> setEndValue(double end) async {
+    final prefs = await _prefs.setDouble(rangeValueEnd, end);
+
+    return prefs;
+  }
+
+  static double getStartValue() {
+    final prefs = _prefs.getDouble(rangeValueStart) ?? 100.0;
+
+    return prefs;
+  }
+
+  static double getEndValue() {
+    final prefs = _prefs.getDouble(rangeValueEnd) ?? 10000.0;
+
+    return prefs;
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ import 'package:places/cubits/distance_slider_cubit/distance_slider_cubit.dart';
 import 'package:places/cubits/image_provider/image_provider_cubit.dart';
 import 'package:places/cubits/places_list/places_list_cubit.dart';
 import 'package:places/cubits/show_places_button/show_places_button_cubit.dart';
+import 'package:places/data/store/app_preferences.dart';
 import 'package:places/providers/theme_data_provider.dart';
 import 'package:places/ui/res/app_strings.dart';
 import 'package:places/ui/screens/onboarding_screen/onboarding_screen.dart';
@@ -27,7 +28,9 @@ final ThemeData _lightTheme = AppTheme.buildTheme();
 final ThemeData _darkTheme = AppTheme.buildThemeDark();
 
 // ignore: long-method
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AppPreferences.init();
   runApp(
     MultiProvider(
       providers: [
