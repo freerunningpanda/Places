2b68a98222d5005198f28c5cb21169e2f768e11e
Aleksandr Nagornyy <freerunningpanda@yandex.ru>
2023-04-27 00:20:44 +0700

    released search_bar

--- lib/ui/screens/map_screen/map_screen.dart
+++ lib/ui/screens/map_screen/map_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:places/cubits/places_list/places_list_cubit.dart';
 import 'package:places/ui/res/app_strings.dart';
 import 'package:places/ui/widgets/search_appbar.dart';
+import 'package:places/ui/widgets/search_bar.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
@@ -12,14 +15,23 @@ class MapScreen extends StatefulWidget {
 class _MapScreenState extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
+    final isSearchPage = context.read<PlacesListCubit>().isSearchPage;
+    final readOnly = context.read<PlacesListCubit>().readOnly;
+
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
         child: Column(
-          children: const [
-            SizedBox(height: 16),
-            SearchAppBar(title: AppStrings.mapScreenTitle),
-            SizedBox(height: 16),
+          children: [
+            const SizedBox(height: 16),
+            const SearchAppBar(title: AppStrings.mapScreenTitle),
+            const SizedBox(height: 16),
+            SearchBar(
+              isMainPage: true,
+              isSearchPage: isSearchPage,
+              readOnly: readOnly,
+              searchController: TextEditingController(),
+            ),
           ],
         ),
       ),
