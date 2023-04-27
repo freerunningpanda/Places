9129920b5cd77b940245160e6f5f3b48df1b0fd9
Aleksandr Nagornyy <freerunningpanda@yandex.ru>
2023-04-27 18:36:11 +0700

    init plugin

--- android/app/src/main/kotlin/nagornyy/places/MainActivity.kt
+++ android/app/src/main/kotlin/nagornyy/places/MainActivity.kt
@@ -1,6 +1,20 @@
-package nagornyy.places
+package com.unact.yandexmapkitexample
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import com.yandex.mapkit.MapKitFactory
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        MapKitFactory.setLocale("YOUR_LOCALE")
+        MapKitFactory.setApiKey("45f6d5c0-c936-48a3-95a2-0c69b71c6ce8")
+        super.configureFlutterEngine(flutterEngine)
+    }
 }
+
+
+
+
+
+
