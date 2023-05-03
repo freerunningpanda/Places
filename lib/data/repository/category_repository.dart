2cd7707cd295744cc6406c835c05032c7a777c8f
Aleksandr Nagornyy <freerunningpanda@yandex.ru>
2023-05-04 01:57:30 +0700

    little changes

--- lib/data/repository/category_repository.dart
+++ lib/data/repository/category_repository.dart
@@ -8,7 +8,7 @@ class CategoryRepository {
     Category(title: AppStrings.hotel, placeType: PlaceType.hotel),
     Category(title: AppStrings.restaurant, placeType: PlaceType.restaurant),
     Category(title: AppStrings.particularPlace, placeType: PlaceType.other),
-    Category(title: AppStrings.theater, placeType: PlaceType.park),
+    Category(title: AppStrings.park, placeType: PlaceType.park),
     Category(title: AppStrings.museum, placeType: PlaceType.museum),
     Category(title: AppStrings.cafe, placeType: PlaceType.cafe),
   ];
